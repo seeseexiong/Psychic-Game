1dd98ae8534b81aa0959e2af8377b9fe511b3c48
See Xiong <seeseexiong@gmail.com>
2019-02-26 00:02:23 -0600

    added style and added logic to accept alphabet letters only

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,69 +1,77 @@
-    // make all the options you can key into an array
-    var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+//avialable options are all the alphabet keys
+var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-    // Create variables to hold the number of wins, losses, guesses left, and guesses so far. Set the values to starting values.
-    var wins = 0;
-    var losses = 0;
-    var gLeft = 9;
-    var gsFar = "";
-    
-    var computerGuess;
+//the data we are keeping track of are: win score, loss score, guess so far, rounds left, computer pick
+var winScore = 0;
+var lossScore = 0;
+var roundLeft = 9;
+var guessSoFar = "";
+var computerPick
 
-    // Create variables that hold references to the places in the HTML where we want to display things.
-   
-    var userChoiceText = document.getElementById("userchoice-text");
-    //var computerChoiceText = document.getElementById("computerchoice-text");
-    var winsText = document.getElementById("wins-text");
-    var lossesText = document.getElementById("losses-text");
-    var gLeftText = document.getElementById("gLeft-text");
-    var gsFarText = document.getElementById("gsFar-text");
+//create variables to hold reference on DOM
+var updateWin = document.getElementById("win-text");
+var updateLoss = document.getElementById("loss-text");
+var updateRoundLeft = document.getElementById("gLeft-text");
 
-        
-    //the event is when the user pressed a key
-    document.onkeyup = function(event) {
-    //capture the key
-    var userGuess = event.key;
-    gsFar += userGuess + ", ";
-      
+//FUNCTIONS goes here
+//update guess left and display on DOM
+function updateGSF() {
+    document.getElementById("gSFar-text").innerHTML = guessSoFar;
+};
 
-            
-        // Computer randomly chooses a choice each round(9 guesses) from the options array.
-        if (gLeft === 9) {
-            computerGuess = options[Math.floor(Math.random() * options.length)];
+//generate random letter
+function randomLetter() {
+    computerPick = options[Math.floor(Math.random()*options.length)]
+    console.log("Computer Pick: " + computerPick);
+}
+
+//MAIN PROCESS STARTS ===========================================================================
+//1.) When the game start, you have 9 rounds, thecomputer pick a letter
+if (roundLeft = 9) {
+    randomLetter();
+}
+
+
+//EVENT
+//2.) when a key is released it will tiger the event
+document.onkeyup = function(event) {
+    // the key which is pressed is the event
+    var keyPressed = event.key.toLowerCase();
+    // the keys pressed are data of variable guessSoFar
+    guessSoFar += (keyPressed + " ,");
+    //display guessSoFar
+    updateGSF();
+
+    //loop through the available options, if the key which is pressed is one of the available options then..
+    for (var i = 0; i<options.length; i++) {
+
+        if ( keyPressed === options[i] ) {
+
+            //if keyPressed is same as computerPick then win score add 1, round left go back to 9, computer pick a new letter
+            if ( keyPressed === computerPick) {
+                winScore++;
+                roundLeft = 9;
+                randomLetter();
+                guessSoFar = "";
+                updateGSF();
+            }
+            //or else, round left minus 1
+            else {
+                roundLeft--;
+                //if round left reaches 0, then user lose one score. Everything goes back to default
+                if (roundLeft === 0) {
+                    lossScore++;
+                    guessSoFar = "";
+                    updateGSF();
+                    randomLetter();
+                    roundLeft = 9;
+                }
+            }
         }
+    }
 
-
-
-        //if userGuess the same as computerGuess then user win one point
-        if (userGuess === computerGuess) {
-            computerGuess = options[Math.floor(Math.random() * options.length)];
-            wins++;
-            gLeft = 9;
-            gsFar = "";
-        }
-
-
-        // if your max guess reach 0 then you lose one point
-        if (gLeft === 0){
-            computerGuess = options[Math.floor(Math.random() * options.length)];
-            losses++;
-            gLeft = 9;
-            gsFar = "";
-        }
-            
-            
-            // Display the user and computer choices to DOM. Call all revelent properties above
-            //hides the directions
-          
-            userChoiceText.textContent = "You choose: " + userGuess;
-            //computerChoiceText.textContent = "Computer choose: " + computerGuess;
-            winsText.textContent = "Wins: " + wins;
-            lossesText.textContent = "Losses: " + losses;
-            gLeftText.textContent = "Guesses left: " + gLeft;
-            gsFarText.textContent = "Your Guesses so far: " + gsFar;
-
-
-            //9 max guesses; each guess is subtract from max guesses
-            gLeft -= 1;
-            
-    };
+    //display the win score, loss score and rounds left onto DOM
+    updateWin.textContent = winScore;
+    updateLoss.textContent = lossScore;
+    updateRoundLeft.textContent = roundLeft;
+}
